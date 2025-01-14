bdcdca27d2022ade7e265a64beff4d05ff9a36d7
Vicorico17 <vic.cazacu@gmail.com>
2025-01-15 01:05:18 +0200

    creca

--- src/app/api/fights/[id]/route.ts
+++ src/app/api/fights/[id]/route.ts
@@ -1,5 +1,5 @@
 import { NextResponse } from 'next/server'
-import { Fight } from '@/app/types/fight'
+import { Fight } from '../../../../../app/types/fight'
 
 // This would typically be replaced with a database
 let fights: Fight[] = [
